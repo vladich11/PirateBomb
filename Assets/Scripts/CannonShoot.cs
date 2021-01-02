ac7eda5853a96b04ae2bd6ea3972af2e70f3ee50
vladich11 <vladche99@gmail.com>
2021-01-02 22:54:07 +0200

    first commit

---
+++ Assets/Scripts/CannonShoot.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CannonShoot : MonoBehaviour
+{
+   public Transform shootPos;
+    public GameObject sphere;
+    public float shootPower;
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+            Instantiate(sphere, shootPos.position, shootPos.rotation).GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * shootPower);    
+    }
+
+}
