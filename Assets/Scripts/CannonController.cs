632b3e40d37b234c6597216ef9081308430ebea5
vladich11 <vladche99@gmail.com>
2021-01-03 11:47:19 +0200

    CannonShoot

---
+++ Assets/Scripts/CannonController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonController : MonoBehaviour
+{
+
+    //Cannon Rotation Variables
+    public int speed;
+    public float friction;
+    public float lerpSpeed;
+    float xDegrees;
+    float yDegrees;
+    Quaternion fromRotation;
+    Quaternion toRotation;
+    Camera camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            if(hit.transform.gameObject.tag == "Cannon")
+            {
+                if (Input.GetMouseButton(1))
+                {
+                    xDegrees -= Input.GetAxis("Mouse Y") * speed * friction;
+                    yDegrees -= Input.GetAxis("Mouse X") * speed * friction;
+                    fromRotation = transform.rotation;
+                    toRotation = Quaternion.Euler(xDegrees, yDegrees, 0);
+                    transform.rotation = Quaternion.Lerp(fromRotation, toRotation, Time.deltaTime * lerpSpeed);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/bombController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bombController : MonoBehaviour
+{
+    public float delay = 3f;
+
+    public GameObject explosionEffect;
+
+    bool hasExploded = false;
+    float countdown;
+
+    void Start()
+    {
+        countdown = delay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        if (countdown <= 0f && !hasExploded)
+        {
+            Explode();
+            hasExploded = true;
+        }
+    }
+
+    void Explode()
+    {
+        
+    Instantiate(explosionEffect, transform.position, transform.rotation);
+            //get nearby objects
+            // add force
+            //damage
+        
+     Destroy(gameObject);
+    }
+}
