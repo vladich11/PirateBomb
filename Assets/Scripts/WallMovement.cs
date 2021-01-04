52dab1dba9b7f6d50587e156b89fbe6bc7e942a5
vladich11 <vladche99@gmail.com>
2021-01-04 22:10:50 +0200

    targetCollision/wallMovement

---
+++ Assets/Scripts/WallMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallMovement : MonoBehaviour
+{
+    private bool dirRight = true;
+    public float speed = 2.0f;
+
+    void Update()
+    {
+        if (dirRight)
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        else
+            transform.Translate(-Vector3.forward * speed * Time.deltaTime);
+
+        if (transform.position.z >= 35f)
+        {
+            dirRight = false;
+        }
+
+        if (transform.position.z <= -15)
+        {
+            dirRight = true;
+        }
+
+    }
+}
---
+++ Assets/Scripts/targetController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class targetController : MonoBehaviour
+{
+    public GameObject bomb;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Bomb")
+        {
+            Debug.LogWarning("target hit");
+            Destroy(gameObject, 1f);
+        }
+    }
+}
